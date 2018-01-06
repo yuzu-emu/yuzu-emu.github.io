3fd3554a1b27101b40ddb8f3b6598039ea0ae137
Chris <chris062689@gmail.com>
2018-01-06 01:48:28 -0500

    Changed navbar from is-light to is-dark, updated SCSS to use dark theme.

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,11 +1,11 @@
-let navbar = document.getElementById("hero-navbar");
+const navbar = document.getElementById('hero-navbar');
 
 if (navbar) {
-    window.addEventListener('scroll', function (e) {
+    window.addEventListener('scroll', () => {
         if (window.scrollY > 0) {
-            navbar.classList.add("is-freestanding");
+            navbar.classList.add('is-freestanding');
         } else {
-            navbar.classList.remove("is-freestanding");
+            navbar.classList.remove('is-freestanding');
         }
     });
 }
