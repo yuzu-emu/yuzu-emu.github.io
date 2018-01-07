8871bdb68c7652f4eb50cbb3abc6da1db7a80749
Chris <chris062689@gmail.com>
2018-01-07 17:21:07 -0500

    Updated partials. Moved scss into their own folders.

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,11 +1,14 @@
 const navbar = document.getElementById('hero-navbar');
+const navbarBaseColor = 'is-dark';
 
 if (navbar) {
     window.addEventListener('scroll', () => {
         if (window.scrollY > 0) {
             navbar.classList.add('is-freestanding');
+            navbar.classList.add(navbarBaseColor);
         } else {
             navbar.classList.remove('is-freestanding');
+            navbar.classList.remove(navbarBaseColor);
         }
     });
 }
--- update_submodules.sh
+++ update_submodules.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+rm -rf ./site/themes/shared-bulma-theme/
 git submodule update --remote --merge
+git add ./site/themes/shared-bulma-theme/
+git commit -m "Updated shared-bulma-theme."
+git push
 
