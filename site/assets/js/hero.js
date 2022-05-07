8f3590994d60e366a3031db3feb044100e2a4650
liushuyu <liushuyu011@gmail.com>
2022-05-07 16:45:00 -0600

    js: use Hugo to bundle, minify and SRI-tagging the JS files

--- site/assets/js/hero.js
+++ site/assets/js/hero.js
@@ -3,7 +3,7 @@ const navbar = document.getElementById('hero-navbar');
 const navbarBaseColor = 'is-dark';
 
 if (navbar) {
-	window.addEventListener('scroll', () => {
+    window.addEventListener('scroll', () => {
         if (window.scrollY > 0) {
             navbar.classList.add('is-freestanding');
             navbar.classList.add(navbarBaseColor);
@@ -15,30 +15,30 @@ if (navbar) {
 }
 
 // Handle random switch colors
-let colors = ["orangered", "deepskyblue", "yellow", "deeppink", "lime", "gray"];
-document.body.classList.add("switch-left-" + colors.splice(Math.floor(Math.random() * colors.length), 1)[0]);
-document.body.classList.add("switch-right-" + colors[Math.floor(Math.random() * colors.length)]);
+let colors = ['orangered', 'deepskyblue', 'yellow', 'deeppink', 'lime', 'gray'];
+document.body.classList.add('switch-left-' + colors.splice(Math.floor(Math.random() * colors.length), 1)[0]);
+document.body.classList.add('switch-right-' + colors[Math.floor(Math.random() * colors.length)]);
 
 // Handle lazy-loading of images
-document.addEventListener("DOMContentLoaded", function () {
-    const elements = document.getElementsByClassName("lazy-load");
+document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.getElementsByClassName('lazy-load');
     for (let i = 0; i < elements.length; i++) {
         const element = elements[i];
-        element.src = element.getAttribute("data-src");
+        element.src = element.getAttribute('data-src');
     }
 });
 
 // Show image carousel, if needed
-const carousels = document.getElementsByClassName("glide");
+const carousels = document.getElementsByClassName('glide');
 if (carousels.length > 0) {
-    document.addEventListener("DOMContentLoaded", function () {
-        const glide = new Glide(".glide", {
-            type: "carousel",
+    document.addEventListener('DOMContentLoaded', function () {
+        const glide = new Glide('.glide', {
+            type: 'carousel',
             perView: 1,
-            focusAt: "center",
+            focusAt: 'center',
             autoplay: 4000,
             gap: 0,
-            animationTimingFunc: "ease-in-out",
+            animationTimingFunc: 'ease-in-out',
             animationDuration: 1000
         });
 
